f041a6f7c7c7b4d9989fd27982e0c6f089b5e587
Erol Kılbüker <erolposta@gmail.com>
2020-05-10 17:32:21 +0300

    logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Xbios Dersler",
   description: "Just playing around",
   themeConfig: {
-    // logo: "assets/img/logo.png",
+    logo: "images/logo.png",
     nav: [
       { text: "Home", link: "/" },
       { text: "Guide", link: "/guide/" },
